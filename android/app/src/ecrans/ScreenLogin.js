01b142217894dbe8b79edaed3c0a5ee5bc4bef3f
Fatma <fatmahocine2014@gmail.com>
2023-04-21 16:29:13 +0200

    Page Login en cours

---
+++ android/app/src/ecrans/ScreenLogin.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+import loginSvg from '../../../assets/images/loginSvg'
+
+const ScreenLogin = () => {
+  return (
+    <View>
+        <loginSvg />
+      <Text>ScreenLogin</Text>
+    </View>
+  )
+}
+
+export default ScreenLogin
