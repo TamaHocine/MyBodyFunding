e6d8376fff99719639d6ffd7da68e1330d24f6f4
Fatma <fatmahocine2014@gmail.com>
2023-04-28 16:05:03 +0200

    Suite page de connexion

--- src/components/CustomButton/CustumButton.js
+++ src/components/CustomButton/CustumButton.js
@@ -1,32 +1,25 @@
 import { View, Text, StyleSheet, Pressable } from 'react-native'
 import React from 'react'
 
-const CustumButton = ({onPress, text, type = "PRIMARY"}) => {
+const CustumButton = ({onPress, text}) => {
   return (
     <Pressable 
     onPress={onPress} 
-    style={styles.container, styles[`container_${type}`]}>
-      <Text style= {styles.text, styles[`text_${type}`]}>{text}</Text>
+    style={styles.container}>
+      <Text style= {styles.text}>{text}</Text>
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
     container: {
+      backgroundColor:'#285A84',
         width: '100%',
         padding: 15,
         marginVertical: 5,
         alignItems: 'center',
         borderRadius: 5,
         
-    },
-
-    container_PRIMARY:{
-      backgroundColor:'#285A84',
-    },
-
-    container_TERTIARY:{
-      color: 'gray',
     },
     
     text:{
--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -6,45 +6,34 @@ import CustumButton from '../../components/CustomButton/CustumButton';
 
 
 const SignInScreen = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
 
-  const onSignInPressed = () =>{
-    console.warn('Sign in');
+  const onSignInPressedF = () =>{
+    console.warn('Connexion Facebook');
   };
 
-  const onForgotPasswordPressed = () =>{
-    console.warn('OnForgotPasswordPressed');
+  const onSignInPressedG = () =>{
+    console.warn('Connexion Google');
   };
 
-
-
+  const onSignInPressedE = () =>{
+    console.warn('Connexion Email');
+  };
 
   return (
     <View>
-      <CustomInput 
-      placeholder="Email" 
-      value={username} 
-      setValue={setUsername}>
-      </CustomInput>
 
-      <CustomInput 
-      placeholder="Mot de passe" 
-      value={password} 
-      setValue={setPassword}
-      secureTextEntry={true}
-      >
-      
-      </CustomInput>
+      <CustumButton text="Connexion Facebook" onPress={onSignInPressedF}></CustumButton>
+      <CustumButton text="Connexion Google" onPress={onSignInPressedG}></CustumButton>
+      <CustumButton text="Connexion E-mail" onPress={onSignInPressedE}></CustumButton>
 
-      <CustumButton text="Connexion" onPress={onSignInPressed}></CustumButton>
-      <CustumButton 
-      text="Mot de passe oublié ?" 
-      onPress={onForgotPasswordPressed} 
-      type= "TERTIARY"
-      >
-
-      </CustumButton>
+      <Text>Vous n'avez pas encore de compte, créer un ?</Text>
+      <Text>En créant un compte, vous accepter nos conditions d'utilisation,
+        notrepolitique de confidentialité et notre politique en matière de cookies.
+        Ces deux dernières politiques décrivent comment nous utilisons vos données
+        pour proposer, améliorer et faire la promotion de nos services et notresite, 
+        ainsi que les droits dont vous disposez pour contrôler cette utilisation et
+        comment les exercer.
+      </Text>
     </View>
   );
 };
@@ -52,9 +41,12 @@ const SignInScreen = () => {
 const styles = StyleSheet.create({
   root: {
     alignItems: 'center',
-    padding: 20,
-    
+    padding: 20, 
+
   },
+  
+
+  
 })
 
 export default SignInScreen
